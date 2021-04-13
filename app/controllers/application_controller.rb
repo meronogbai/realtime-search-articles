9868e8005836614bbe36fafd84de1c713904f83e
Meron Ogbai <okbaymeron@gmail.com>
2021-04-13 17:53:25 +0300

    Add tracking cookie logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_tracking_cookie
+
+  private
+
+  def set_tracking_cookie
+    unless cookies[:user_uuid]
+      uuid = SecureRandom.uuid
+      cookies.permanent[:user_uuid] = uuid
+      User.create(uuid: uuid)
+    end
+  end
 end
