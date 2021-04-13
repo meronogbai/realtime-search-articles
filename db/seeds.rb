7c11901d1f6f1405cb2d1508574087bd05470fd6
Meron Ogbai <okbaymeron@gmail.com>
2021-04-14 02:24:26 +0300

    Seed the db with some articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Article.create!([
+  { title: 'Hello world how are you?', body: 'Lorem ipsum dolor sit amet.' },
+  { title: 'What is a good car?', body: 'Lorem ipsum dolor sit amet.' },
+  { title: 'How is Emil Hajric doing?', body: 'Lorem ipsum dolor sit amet.' }
+])
