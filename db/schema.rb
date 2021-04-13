751ee9ba576dd0458eda44d4c1a8eb46fa9ca967
Meron Ogbai <okbaymeron@gmail.com>
2021-04-13 04:37:48 +0300

    Add search and user models and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_223649) do
+ActiveRecord::Schema.define(version: 2021_04_13_013609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,4 +49,19 @@ ActiveRecord::Schema.define(version: 2021_04_12_223649) do
     t.index ["title"], name: "index_articles_on_title", unique: true
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uuid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "searches", "users"
 end
