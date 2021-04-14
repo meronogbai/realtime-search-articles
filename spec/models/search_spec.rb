ceaec29e03c8bc723e1e9d22096d6278b1a66057
Meron Ogbai <okbaymeron@gmail.com>
2021-04-14 06:29:19 +0300

    Add search specs for deleting intermediate queries

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    uuid = SecureRandom.uuid
+    @user = User.create(uuid: uuid)
+  end
+
+  it 'downcases queries' do
+    search = @user.searches.create(query: 'HELLO')
+    expect(search.query).to eq 'hello'
+  end
+
+  it 'deletes previous search if new query starts with previous query' do
+    @user.searches.create([
+      { query: 'What' },
+      { query: 'What is a' },
+      { query: 'What is a good car?' },
+    ])
+    expect(@user.searches.count).to eq 1
+    expect(@user.searches.first.query).to eq 'what is a good car?'
+  end
+
+  it 'deletes previous search if new query starts with previous query and ignores whitespace' do
+    @user.searches.create([
+      { query: 'How is' },
+      { query: 'Howis emil hajric' },
+      { query: 'How is emil hajric doing?' },
+    ])
+    expect(@user.searches.count).to eq 1
+    expect(@user.searches.first.query).to eq 'how is emil hajric doing?'
+  end
+
+  it 'deletes previous search if new query starts with previous query and case whitespace' do
+    @user.searches.create([
+      { query: 'hello' },
+      { query: 'Hello world' },
+      { query: 'Hello world how are you?' },
+    ])
+    expect(@user.searches.count).to eq 1
+    expect(@user.searches.first.query).to eq 'hello world how are you?'
+  end
 end
