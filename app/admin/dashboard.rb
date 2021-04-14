42a1cb5c93971258647c449ff81a03399a60d445
Meron Ogbai <okbaymeron@gmail.com>
2021-04-15 00:18:51 +0300

    Add searches pie chart to active admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,11 +2,9 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
+    h2 'Top Searches'
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+      pie_chart Search.group(:query).order(count: :desc).limit(5).count
     end
 
     # Here is an example of a simple dashboard with columns and panels.
