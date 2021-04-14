f419a00633563502bf1c3ec0064e91c7136e3438
Meron Ogbai <okbaymeron@gmail.com>
2021-04-14 19:02:50 +0300

    Improve article search controller

--- app/controllers/article_search_controller.rb
+++ app/controllers/article_search_controller.rb
@@ -5,8 +5,12 @@ class ArticleSearchController < ApplicationController
   def search
     user = User.find_by(uuid: cookies[:user_uuid])
     query = params[:query]
-    user.searches.create(query: query)
-    @articles = Article.search_with_text(query)
-    render json: { articles: @articles }
+    search = user.searches.build(query: query)
+    if search.save
+      @articles = Article.search_with_text(query)
+      render json: { articles: @articles }, status: :ok
+    else
+      render json: { errors: searh.errors.full_messages}, status: :not_acceptable
+    end
   end
 end
