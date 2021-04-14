8c04a13ba6f14c9aded3c83755d6bdd8db59a2a9
Meron Ogbai <okbaymeron@gmail.com>
2021-04-14 21:47:43 +0300

    Fix article search controller

--- app/controllers/article_search_controller.rb
+++ app/controllers/article_search_controller.rb
@@ -5,12 +5,8 @@ class ArticleSearchController < ApplicationController
   def search
     user = User.find_by(uuid: cookies[:user_uuid])
     query = params[:query]
-    search = user.searches.build(query: query)
-    if search.save
-      @articles = Article.search_with_text(query)
-      render json: { articles: @articles }, status: :ok
-    else
-      render json: { errors: searh.errors.full_messages}, status: :not_acceptable
-    end
+    search = user.searches.create(query: query)
+    @articles = Article.search_with_text(query)
+    render json: { articles: @articles }
   end
 end
