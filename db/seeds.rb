187f63339376b13fac64c2fb86c00296fca67c72
Meron Ogbai <okbaymeron@gmail.com>
2021-04-15 05:41:10 +0300

    Update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 Article.create!([
   { title: 'Hello world how are you?', body: 'Lorem ipsum dolor sit amet.' },
   { title: 'What is a good car?', body: 'Lorem ipsum dolor sit amet.' },
-  { title: 'How is Emil Hajric doing?', body: 'Lorem ipsum dolor sit amet.' }
+  { title: 'How is Emil Hajric doing?', body: 'Lorem ipsum dolor sit amet.' },
+  { title: 'Movies', body: 'I love watching movies.' }
 ])
