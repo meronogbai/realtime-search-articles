2cebda617916ca480fb02435e2caa4943e525bfb
Meron Ogbai <okbaymeron@gmail.com>
2021-04-15 10:47:17 +0300

    Add pagination for articles and searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
   before_action :authenticate_admin_user!
 
   def index
-    @searches = Search.select(:query).distinct
+    @searches = Search.select(:query).distinct.paginate(page: params[:page], per_page: 10)
   end
 
   def top
